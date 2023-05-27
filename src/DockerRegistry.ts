9ad3b9b41ff11d6668614afb11e3fa337c8d2885
Anna Aitchison <anna11199810@hotmail.co.uk>
2023-05-27 23:13:26 +0100

    Add fetch

--- src/DockerRegistry.ts
+++ src/DockerRegistry.ts
@@ -1,17 +1,25 @@
+import fetch from 'node-fetch';
+
 export default class DockerRegistry {
     private url: string;
     private port:number; 
     private username:string;
     private password:string;
     private ignoreSslErrors:boolean;
+    private isHttps:boolean;
 
-    constructor(url: string, port:number, username:string, password:string, ignoreSslErrors:boolean) {
+    constructor(url="localhost", port=5000, username="", password="", ignoreSslErrors=true, isHttps=true) {
         this.url = url;
         this.port = port;
         this.username = username;
         this.password = password;
         this.ignoreSslErrors = ignoreSslErrors;
-        
+        this.isHttps = isHttps;
+    }
+
+    private async makeRegistryRequest() {
+        let result = await fetch();
+        return false;
     }
 
     test() {
